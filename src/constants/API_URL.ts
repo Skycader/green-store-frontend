310597e08a634f643c8cdf982c22063e9694bc00
skycader <skycader@github.com>
2022-11-14 00:56:05 +0300

    Now single

--- src/constants/API_URL.ts
+++ src/constants/API_URL.ts
@@ -1,3 +1,3 @@
-// const API_URL: string = "http://localhost:1337/"
-const API_URL: string = "https://green-store-backend.herokuapp.com/"
+const API_URL: string = "http://localhost:1337/"
+//const API_URL: string = "https://green-store-backend.herokuapp.com/"
 export default API_URL
